5766d66fd691b9d818f0042c031e85ffb2c6e264
isayabs <isabelleyabut@gmail.com>
2026-04-15 04:53:16 -0600

    Backend improvements and bug fixes

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
